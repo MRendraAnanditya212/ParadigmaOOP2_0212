de439f4c1dfda35381658574c56ebf3872f0bbca
MRendraAnanditya212 <mrendraananditya@gmail.com>
2025-06-02 18:54:14 +0700

    membuat class seseorang

---
+++ polimorpi.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+
+
+class joko : public seseorang 
+{
+public:
+    void pesan()
+    {
+        cout << "Pesan dari lia" << endl;
+    }
+};
+
+class lia :  public seseorang
+{
+public:
+    void pesan()
+    {
+        cout << "Pesan dari lia" << endl;
+    }
+};
+
+int main()
+{
+    seseorang *obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    // a.seseorang::pesan();
+    // b.seseorang::pesan();
+
+    return 0;
+}
